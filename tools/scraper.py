387e6f85bd108ed924f9048cbd6aee81a46524db
Viraj Mehta <thebigmehtaphor@gmail.com>
2017-03-01 11:34:10 -0800

    added robustness to page parse

--- tools/scraper.py
+++ tools/scraper.py
@@ -9,7 +9,10 @@ def parse_one_letter(letter, db):
     # for each elt in the list, elt.string gives the word
 
 def parse_one_page(url):
-    soup= bs(requests.get(url))
+    page = requests.get(url)
+    if page.status_code != 200:
+        return None, None
+    soup= bs(page.text)
     clue = soup.find('strong').string
     words = [elt.string for elt in soup.find_all('div', class_='word')]
     return clue, words
