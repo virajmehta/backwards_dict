db7cb2edcd1b88598c869edf35e5c18f78335e89
Meena Chetty <meenachetty@Meenas-MacBook-Pro.local>
2017-03-11 15:21:33 -0800

    stacked changes

--- stacked_lstm_model.py
+++ stacked_lstm_model.py
@@ -168,13 +168,8 @@ class StackedLSTMModel(Model):
         data = WrapperClass()
         prog = Progbar(target=1 + data.num_crossword_examples / self.config.batch_size)
         for _ in range(int(data.num_crossword_examples / self.config.batch_size)):
-            batch = data.get_crossword_batch(dimensions=self.config.batch_size)
-            dict_batch = data.get_dictionary_batch(dimensions=self.config.batch_size)
-            if len(dict_batch) == 0:
-                dict_batch = data.get_dictionary_batch(dimensions=self.config.batch_size)
-
-            loss = self.train_on_batch(sess, batch) #TODO
-            loss += self.train_on_batch(sess, dict_batch)
+            loss = self.train_on_batch(sess, data, True) #TODO
+            loss += self.train_on_batch(sess, data, False)
             prog.update(_ + 1, [("train loss", loss)])
             if self.report: self.report.log_train_loss(loss)
         print("")
@@ -188,7 +183,7 @@ class StackedLSTMModel(Model):
         #TODO
         logger.info("Evaluating on development data")
         accuracy = self.evaluate_dev_set(sess)
-        print 'Accuracy on dev set: {}'.format(accuracy)
+        logger.info('Accuracy on dev set: {}'.format(accuracy))
         return accuracy
 
     def evaluate_dev_set(self, sess):
